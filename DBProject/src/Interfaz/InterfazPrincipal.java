82b4e70283b0aa5faeab89cd9398aa0f8e40aec2
garzuzo <garzuxo@gmail.com>
2018-04-16 11:22:32 -0500

    .

--- DBProject/src/Interfaz/InterfazPrincipal.java
+++ DBProject/src/Interfaz/InterfazPrincipal.java
@@ -1,10 +1,13 @@
 package Interfaz;
 
-public class InterfazPrincipal {
+import javax.swing.JFrame;
+
+public class InterfazPrincipal extends JFrame{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+InterfazPrincipal ip=new InterfazPrincipal();
+ip.setVisible(true);
 	}
 
 }

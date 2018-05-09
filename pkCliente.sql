e78a125e8cbae193d9d07d5917d48d2bdfb50140
juan <juanfe_518@hotmail.com>
2018-05-09 10:50:48 -0500

    Update pkCliente.sql

--- pkCliente.sql
+++ pkCliente.sql
@@ -1,55 +1,55 @@
---creo que esto va a parte, la creacion y eliminacion de la tabla
---no se si va aqui en este package como tal
-DROP TABLE Cliente CASCADE constraints;
-CREATE TABLE Cliente (
-	cedula NUMBER(10),
-	nombre VARCHAR2(50),
-    direccion  VARCHAR2(100),
-	PRIMARY KEY(cedula)
-);
 
-CREATE OR REPLACE PACKAGE pkCliente IS
+CREATE OR REPLACE PACKAGE pkCliente AS
 PROCEDURE insertar(cedula NUMBER, nombre VARCHAR2, direccion VARCHAR2);
 PROCEDURE eliminar(cedula NUMBER);
 PROCEDURE actualizarNombre(cedula NUMBER, nombre VARCHAR2);
 PROCEDURE actualizarDireccion(cedula NUMBER, direccion VARCHAR2);
-FUNCTION  buscarCliente(cedula NUMBER) RETURN TABLE;
+FUNCTION  buscarCliente(cedula NUMBER) RETURN cliente%rowtype;
 END pkCliente;
-
-CREATE OR REPLACE PACKAGE BODY pkCliente IS
+/
+CREATE OR REPLACE PACKAGE BODY pkCliente AS
 
 PROCEDURE insertar(cedula NUMBER, nombre VARCHAR2, direccion VARCHAR2) IS
 BEGIN 
 INSERT INTO Cliente VALUES(cedula, nombre, direccion);
-EXCEPTION
-WHEN OTHERS THEN
-    raise_application_error(-20000, 'Error insertando en tabla Cliente' || ERRMSG);
+--EXCEPTION
+--WHEN OTHERS THEN
+--    raise_application_error(-20000, 'Error insertando en tabla Cliente' || ERRMSG);
 END insertar;
 
-PROCEDURE eliminar(ced NUMBER) IS
+PROCEDURE eliminar(cedula NUMBER) IS
 BEGIN
-DELETE FROM Cliente WHERE cedula = ced;
+DELETE FROM Cliente c WHERE c.cedula = cedula;
 END eliminar;
 
-PROCEDURE actualizarNombre(ced NUMBER, nom VARCHAR2) IS
+PROCEDURE actualizarNombre(cedula NUMBER, nombre VARCHAR2) IS
 BEGIN
-UPDATE Cliente SET nombre = nom WHERE cedula= ced;
+UPDATE Cliente c SET c.nombre = nombre WHERE c.cedula= cedula;
 END actualizarNombre;
 
-PROCEDURE actualizarDireccion(ced NUMBER,dir VARCHAR2) IS
+PROCEDURE actualizarDireccion(cedula NUMBER,direccion VARCHAR2) IS
 BEGIN
-UPDATE Cliente SET direccion = dir WHERE cedula=ced;
+UPDATE Cliente c SET c.direccion = direccion WHERE c.cedula=cedula;
 END actualizarDireccion;
 
-FUNCTION buscarCliente(ced NUMBER) RETURN TABLE IS
+
+
+--TODO: fix rowtype
+FUNCTION buscarCliente(cedula NUMBER) RETURN cliente%rowtype   IS
 --DECLARACION VARIABLES
 --datosCliente VARCHAR2(200);
+cli cliente%rowtype;
 BEGIN
-SELECT *
-FROM Cliente
-WHERE cedula=ced;
+SELECT * into cli
+FROM Cliente c
+WHERE c.cedula=cedula;
+
+RETURN cli;
+
 END buscarCliente;
 
 END pkCliente;
+/
 
 EXECUTE pkCliente.insertar(1234, 'Sandra', 'aqui');
+

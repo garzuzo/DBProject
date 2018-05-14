39060bcdafebdff9ae790794a68c13fb151d51be
Ronaldo Rodríguez <36302478+ronaldo3r@users.noreply.github.com>
2018-05-14 17:43:46 -0500

    pkFuncionario corregido

--- pkNivel1/pkFuncionario.sql
+++ pkNivel1/pkFuncionario.sql
@@ -1,39 +1,46 @@
-CREATE OR REPLACE PACKAGE pkFuncionario IS
+CREATE OR REPLACE PACKAGE pkFuncionario AS
 PROCEDURE insertar(id NUMBER, nombre VARCHAR2);
 PROCEDURE eliminar(id NUMBER);
 PROCEDURE actualizarNombre(id NUMBER, nombre VARCHAR2);
-FUNCTION  buscarFuncionario(id NUMBER) RETURN TABLE;
+FUNCTION  buscarFuncionario(id NUMBER) RETURN funcionario%rowtype;
 END pkFuncionario;
 
-CREATE OR REPLACE PACKAGE BODY pkFuncionario IS
+/
+CREATE OR REPLACE PACKAGE BODY pkFuncionario AS
 
 PROCEDURE insertar(id NUMBER, nombre VARCHAR2) IS
 BEGIN 
-INSERT INTO funcionario VALUES(id, nombre);
-EXCEPTION
-WHEN OTHERS THEN
-    raise_application_error(-20000, 'Error insertando en tabla Cliente' || ERRMSG);
+INSERT INTO Funcionario VALUES(id, nombre);
+--EXCEPTION
+--WHEN OTHERS THEN
+--    raise_application_error(-20000, 'Error insertando en tabla Cliente' || ERRMSG);
 END insertar;
 
-PROCEDURE eliminar(Sid NUMBER) IS
+PROCEDURE eliminar(id NUMBER) IS
 BEGIN
-DELETE FROM funcionario WHERE id = Sid;
+DELETE FROM Funcionario fun WHERE fun.id = id;
 END eliminar;
 
-PROCEDURE actualizarNombre(Sid NUMBER, nom VARCHAR2) IS
+PROCEDURE actualizarNombre(id NUMBER, nombre VARCHAR2) IS
 BEGIN
-UPDATE funcionario SET nombre = nom WHERE id= Sid;
+UPDATE Funcionario fun SET fun.nombre = nombre WHERE fun.id= id;
 END actualizarNombre;
 
-FUNCTION buscarFuncionario(Sid NUMBER) RETURN TABLE IS
+FUNCTION buscarFuncionario(id NUMBER) RETURN funcionario%rowtype IS
 --DECLARACION VARIABLES
 --datosFuncionario VARCHAR2(200);
+func funcionario%rowtype;
 BEGIN
-SELECT *
-FROM funcionario
-WHERE id=Sid;
+SELECT * into func
+FROM Funcionario fun
+WHERE fun.id=id;
+
+RETURN func;
+
 END buscarFuncionario;
 
 END pkFuncionario;
+/
 
 EXECUTE pkFuncionario.insertar(1234, 'Ronaldo');
+EXECUTE pkFuncionario.eliminar(1234);

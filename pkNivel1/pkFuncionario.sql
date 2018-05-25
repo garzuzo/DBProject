551ff485298e595be6aee12f2b5f42f9f15078f7
Ronaldo Rodríguez <36302478+ronaldo3r@users.noreply.github.com>
2018-05-25 13:59:19 -0500

    pkFuncionario excepciones

--- pkNivel1/pkFuncionario.sql
+++ pkNivel1/pkFuncionario.sql
@@ -11,19 +11,25 @@ CREATE OR REPLACE PACKAGE BODY pkFuncionario AS
 PROCEDURE insertar(id NUMBER, nombre VARCHAR2) IS
 BEGIN 
 INSERT INTO Funcionario VALUES(id, nombre);
---EXCEPTION
---WHEN OTHERS THEN
---    raise_application_error(-20000, 'Error insertando en tabla Cliente' || ERRMSG);
+EXCEPTION
+WHEN OTHERS THEN
+    raise_application_error(-20000, 'Error insertando en tabla Funcionario' ||SQLCODE);
 END insertar;
 
 PROCEDURE eliminar(id NUMBER) IS
 BEGIN
 DELETE FROM Funcionario fun WHERE fun.id = id;
+EXCEPTION
+WHEN OTHERS THEN
+    raise_application_error(-20000, 'Error insertando en tabla Funcionario' ||SQLCODE);
 END eliminar;
 
 PROCEDURE actualizarNombre(id NUMBER, nombre VARCHAR2) IS
 BEGIN
 UPDATE Funcionario fun SET fun.nombre = nombre WHERE fun.id= id;
+EXCEPTION
+WHEN OTHERS THEN
+    raise_application_error(-20000, 'Error insertando en tabla Funcionario' ||SQLCODE);
 END actualizarNombre;
 
 FUNCTION buscarFuncionario(id NUMBER) RETURN funcionario%rowtype IS
@@ -34,13 +40,13 @@ BEGIN
 SELECT * into func
 FROM Funcionario fun
 WHERE fun.id=id;
-
 RETURN func;
+
+EXCEPTION
+WHEN OTHERS THEN
+    raise_application_error(-20000, 'Error insertando en tabla Funcionario' ||SQLCODE);
 
 END buscarFuncionario;
 
 END pkFuncionario;
 /
-
-EXECUTE pkFuncionario.insertar(1234, 'Ronaldo');
-EXECUTE pkFuncionario.eliminar(1234);

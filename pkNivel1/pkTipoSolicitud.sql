a8061cf86be28d1ed6d1a7dd5211774571906d6f
juan <juanfe_518@hotmail.com>
2018-05-25 11:11:20 -0500

    Exception handling tipo_producto y tipo_Solicitud

--- pkNivel1/pkTipoSolicitud.sql
+++ pkNivel1/pkTipoSolicitud.sql
@@ -12,6 +12,11 @@ CREATE OR REPLACE PACKAGE BODY pkTipoSolicitud AS -- creacion cuerpo del paquete
 PROCEDURE pInsertarTipoSolicitud(idTSol NUMBER, nomTSol varchar) is
 BEGIN
     INSERT INTO TIPOSOlICITUD (id, nombre) VALUES (idTSol, nomTSol);
+
+    EXCEPTION
+    WHEN OTHERS THEN
+    RAISE_APPLICATION_ERROR(-20000,'Error al insertar en la tabla Cliente.'||SQLERRM);
+    END pEliminarTipoProducto;
 END pInsertarTipoSolicitud;
 
 --Metodo para consultar un tipo de solicitud existente en el sistema
@@ -20,6 +25,11 @@ FUNCTION fConsultarTipoSolicitud(idTSol NUMBER) return TIPOSOlICITUD%ROWTYPE is
 BEGIN
     SELECT * INTO tipoSol FROM TIPOSOlICITUD ts WHERE ts.id = idTSol;
     return tipoSol;
+
+    EXCEPTION
+    WHEN OTHERS THEN
+    RAISE_APPLICATION_ERROR(-20000,'Error al insertar en la tabla Cliente.'||SQLERRM);
+    END pEliminarTipoProducto;
 END fConsultarTipoSolicitud;
 
 --Metodo para modificar un tipo de solicitud existente en el sistema
@@ -28,12 +38,22 @@ BEGIN
     UPDATE TIPOSOlICITUD
     SET nombre = nomTSol
     WHERE id = idTSol;
+
+    EXCEPTION
+    WHEN OTHERS THEN
+    RAISE_APPLICATION_ERROR(-20000,'Error al insertar en la tabla Cliente.'||SQLERRM);
+    END pEliminarTipoProducto;
 END pModififcarTipoSolicitud;
 
 --Metodo para eliminar un tipo de solicitud en el sistema
 PROCEDURE pEliminarTipoSolicitud(idTSol NUMBER) is
 BEGIN
     DELETE FROM TIPOSOlICITUD WHERE id=idTSol;
+
+    EXCEPTION
+    WHEN OTHERS THEN
+    RAISE_APPLICATION_ERROR(-20000,'Error al insertar en la tabla Cliente.'||SQLERRM);
+    END pEliminarTipoProducto;
 END pEliminarTipoSolicitud;
 END pkTipoSolicitud;
 
--- pkNivel1/pktipoProducto.sql
+++ pkNivel1/pktipoProducto.sql
@@ -9,6 +9,10 @@ CREATE OR REPLACE PACKAGE BODY pktipoProducto AS -- body
 PROCEDURE pInsertarTipoProducto(idTProd NUMBER, nomTProd varchar) is
 BEGIN
     INSERT INTO TIPO_PRODUCTO (id, NOMBRE_TIPO_PRODUCTO) VALUES (idTProd, nomTProd);
+
+    EXCEPTION
+    WHEN OTHERS THEN
+    RAISE_APPLICATION_ERROR(-20000,'Error al insertar en la tabla Cliente.'||SQLERRM);
 END pInsertarTipoProducto;
 
 
@@ -17,6 +21,10 @@ FUNCTION fConsultarTipoProducto(idTProd NUMBER) return TIPO_PRODUCTO%ROWTYPE is
 BEGIN
     SELECT * INTO tipoProd FROM TIPO_PRODUCTO tp WHERE tp.id = idTProd;
     return tipoProd;
+
+    EXCEPTION
+    WHEN OTHERS THEN
+    RAISE_APPLICATION_ERROR(-20000,'Error al insertar en la tabla Cliente.'||SQLERRM);
 END fConsultarTipoProducto;
 
 PROCEDURE pModififcarTipoProducto(idTProd NUMBER, nomTProd varchar) is
@@ -24,12 +32,20 @@ BEGIN
     UPDATE TIPO_PRODUCTO tp
     SET  tp.NOMBRE_TIPO_PRODUCTO = nomTProd
     WHERE tp.id = idTProd;
+
+    EXCEPTION
+    WHEN OTHERS THEN
+    RAISE_APPLICATION_ERROR(-20000,'Error al insertar en la tabla Cliente.'||SQLERRM);
 END pModififcarTipoProducto;
 
 PROCEDURE pEliminarTipoProducto(idTProd NUMBER) is
 BEGIN
     DELETE FROM TIPO_PRODUCTO WHERE id=idTProd;
-END pEliminarTipoProducto;
+
+    EXCEPTION
+    WHEN OTHERS THEN
+    RAISE_APPLICATION_ERROR(-20000,'Error al insertar en la tabla Cliente.'||SQLERRM);
+    END pEliminarTipoProducto;
 END pktipoProducto;
 
 --pruebas procedimientos

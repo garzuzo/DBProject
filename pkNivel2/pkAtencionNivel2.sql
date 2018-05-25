ac81cded8848e958ba4d15064c3bdadd3f7cbfbe
juan <juanfe_518@hotmail.com>
2018-05-25 14:31:21 -0500

    Add funcionDanosReclamos

--- pkNivel2/pkAtencionNivel2.sql
+++ pkNivel2/pkAtencionNivel2.sql
@@ -2,7 +2,7 @@
 CREATE OR REPLACE PACKAGE pkAtencionNivel2 AS -- spec
 PROCEDURE pSolicitudNuevoProducto(idTSol NUMBER, nomTSol varchar);
 PROCEDURE pSolicitudRetiroProducto(idSolicitud NUMBER, observacion VARCHAR) ;
-PROCEDURE pSolicitudDanosReclamos(idTSol NUMBER, nomTSol varchar);
+PROCEDURE pSolicitudDanosReclamos(idSolicitud NUMBER, observacion VARCHAR, estado VARCHAR);
 END pkAtencionNivel2;
 /
 
@@ -29,15 +29,33 @@ BEGIN
         
         --Actualiza el estado de la solicitud.
         PKSOLICITUD.PMODIFICARSOLICITUD(solic.ID, observacion, solic.FECHA_SOLICITUD, SYSDATE, 
-                                'ATENDIDA', solic.CLIENTE_CEDULA, solic.TIPOSOLICITUD_ID, solic.FUNCIONARIO_ID);
-        
-    
+                                'ATENDIDA', solic.CLIENTE_CEDULA, solic.TIPOSOLICITUD_ID, solic.FUNCIONARIO_ID);     
     
 END pSolicitudRetiroProducto;
 
 
 --Procedimiento para atender danos y reclamos
-PROCEDURE pSolicitudDanosReclamos(idTSol NUMBER, nomTSol varchar);
+PROCEDURE pSolicitudDanosReclamos(idSolicitud NUMBER, observacion VARCHAR, estado VARCHAR) IS
+solic SOLICITUD%rowtype;
+BEGIN
+         
+        IF idSolicitud IS NULL THEN
+        RAISE_APPLICATION_ERROR (-20001, 'ingrese el id de la solicitud');
+        
+        IF observacion IS NULL THEN
+        RAISE_APPLICATION_ERROR (-20001, 'ingrese observacion de la solicitud');
+        
+        IF estado IS NULL THEN
+        RAISE_APPLICATION_ERROR (-20001, 'ingrese un estado para la solicitud');
+        
+        --obtiene la solicitud
+        solic := PKSOLICITUD.FCONSULTARSOLICITUD(idSolicitud);        
+            
+        --Actualiza el estado de la solicitud.
+        PKSOLICITUD.PMODIFICARSOLICITUD(solic.ID, observacion, solic.FECHA_SOLICITUD, SYSDATE, 
+                                estado, solic.CLIENTE_CEDULA, solic.TIPOSOLICITUD_ID, solic.FUNCIONARIO_ID);
+        
+END pSolicitudDanosReclamos;
 
 
 END pkAtencionNivel2;

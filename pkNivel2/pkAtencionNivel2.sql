8a7d29e3482f2da8822978cbe5f4c5fa536cf9d5
juan <juanfe_518@hotmail.com>
2018-05-26 22:24:06 -0500

    fixed Atencion2

--- pkNivel2/pkAtencionNivel2.sql
+++ pkNivel2/pkAtencionNivel2.sql
@@ -19,6 +19,8 @@ PROCEDURE pSolicitudNuevoProducto(
     idSolicitud NUMBER)
 IS
   fechaInicio DATE;
+  mes NUMBER;
+  anio VARCHAR(4);
   solic SOLICITUD%rowtype;
 BEGIN
 --valida el parametro
@@ -32,18 +34,23 @@ BEGIN
   --Actualiza el estado del producto a activo
   PKPRODUCTO.PACTUALIZARESTADO( solic.PRODUCTO_ID_PRODUCTO , 'ACTIVO');
   
+  mes :=TO_NUMBER(TO_CHAR(SYSDATE, 'mm'));
+  mes := mes +1;  
+  anio := TO_CHAR(SYSDATE,'yyyy');
+  
   --setea la fecha inicio para el primer dia del mes siquiente al actual.
-  fechaInicio := TO_DATE('''' || EXTRACT(YEAR, SYSDATE) || (EXTRACT(MONTH,
-  SYSDATE))+1 || '1' || '''', 'yyyy/mm/dd');
+  fechaInicio := TO_DATE('''' || anio || '/' || mes || '/1' || '''', 'yyyy/mm/dd');
   
   --Actualiza al fecha de inicio 
   PKPRODUCTO.PACTUALIZARFECHAINICIO( solic.PRODUCTO_ID_PRODUCTO , fechaInicio);
   
-  --Actualiza el estado de la solicitud.
-  PKSOLICITUD.PMODIFICARSOLICITUD(solic.ID, observacion, solic.FECHA_SOLICITUD,
-  SYSDATE, 'ATENDIDA', solic.CLIENTE_CEDULA, solic.TIPOSOLICITUD_ID,
-  solic.FUNCIONARIO_ID);
+   --Actualiza el estado de la solicitud.
+  PKSOLICITUD.PMODIFICARSOLICITUD(solic.ID_SOLICITUD, solic.OBSERVACION, solic.FECHA_SOLICITUD,
+  SYSDATE, 'ATENDIDA', solic.CLIENTE_CEDULA, solic.TIPOSOLICITUD_ID_TIPO_S,
+  solic.FUNCIONARIO_ID_FUNCIONARIO);
 END pSolicitudNuevoProducto;
+
+
 --Procedimiento para atender una solicitud de retiro
 PROCEDURE pSolicitudRetiroProducto(
     idSolicitud NUMBER,
@@ -65,15 +72,16 @@ BEGIN
   PKPRODUCTO.PACTUALIZARESTADO( solic.PRODUCTO_ID_PRODUCTO , 'INACTIVO');
   
   --Actualiza fecha de retiro de producto
-  PKPRODUCTO.PACTUALIZARFECHARETIRO(solic.PRODUCTO_ID, SYSDATE);
+  PKPRODUCTO.PACTUALIZARFECHARETIRO(solic.PRODUCTO_ID_PRODUCTO, SYSDATE);
   
   --Actualiza el estado de la solicitud.
-  PKSOLICITUD.PMODIFICARSOLICITUD(solic.ID, observacion, solic.FECHA_SOLICITUD,
-  SYSDATE, 'ATENDIDA', solic.CLIENTE_CEDULA, solic.TIPOSOLICITUD_ID,
-  solic.FUNCIONARIO_ID);
-  
+  PKSOLICITUD.PMODIFICARSOLICITUD(solic.ID_SOLICITUD, observacion, solic.FECHA_SOLICITUD,
+  SYSDATE, 'ATENDIDA', solic.CLIENTE_CEDULA, solic.TIPOSOLICITUD_ID_TIPO_S,
+  solic.FUNCIONARIO_ID_FUNCIONARIO);  
   
 END pSolicitudRetiroProducto;
+
+
 --Procedimiento para atender danos y reclamos
 PROCEDURE pSolicitudDanosReclamos(
     idSolicitud NUMBER,
@@ -93,9 +101,10 @@ BEGIN
   END IF;
   --obtiene la solicitud
   solic := PKSOLICITUD.FCONSULTARSOLICITUD(idSolicitud);
-  --Actualiza el estado de la solicitud.
-  PKSOLICITUD.PMODIFICARSOLICITUD(solic.ID, observacion, solic.FECHA_SOLICITUD,
-  SYSDATE, estado, solic.CLIENTE_CEDULA, solic.TIPOSOLICITUD_ID,
-  solic.FUNCIONARIO_ID);
+   --Actualiza el estado de la solicitud.
+  PKSOLICITUD.PMODIFICARSOLICITUD(solic.ID_SOLICITUD, observacion, solic.FECHA_SOLICITUD,
+  SYSDATE, estado, solic.CLIENTE_CEDULA, solic.TIPOSOLICITUD_ID_TIPO_S,
+  solic.FUNCIONARIO_ID_FUNCIONARIO);
+  
 END pSolicitudDanosReclamos;
 END pkAtencionNivel2;

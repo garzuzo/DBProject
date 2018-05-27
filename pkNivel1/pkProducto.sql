a09e049e040bd36c14ab63d9176894787f555d3e
garzuzo <garzuxo@gmail.com>
2018-05-27 12:22:28 -0500

    nuevo pkProducto

--- pkNivel1/pkProducto.sql
+++ pkNivel1/pkProducto.sql
@@ -1,10 +1,8 @@
 CREATE OR REPLACE PACKAGE pkProducto AS
 PROCEDURE pInsertar(id NUMBER, fecha_inicio DATE, fecha_retiro DATE, estado_producto VARCHAR2,cedula_cliente NUMBER, tipo_producto NUMBER);
 PROCEDURE pEliminar(id NUMBER);
-PROCEDURE pActualizarFechaInicio(id NUMBER, fecha_inicio DATE);
-PROCEDURE pActualizarFechaRetiro(id NUMBER, fecha_retiro DATE);
-PROCEDURE pActualizarEstado(id NUMBER, estado_producto VARCHAR2);
-FUNCTION  fBuscarProducto(id NUMBER) RETURN producto%rowtype;
+PROCEDURE pModificar(id NUMBER, fecha_inicio DATE);
+FUNCTION  fConsultar(id NUMBER) RETURN producto%rowtype;
 END pkProducto;
 /
 create or replace PACKAGE BODY pkProducto AS
@@ -25,32 +23,17 @@ WHEN OTHERS THEN
 RAISE_APPLICATION_ERROR(-20000,'Error al eliminar en la tabla Producto.'||SQLCODE);
 END pEliminar;
 
-PROCEDURE pActualizarFechaInicio(id NUMBER, fecha_inicio DATE) IS
+
+PROCEDURE pModificar(id NUMBER, fecha_inicio DATE, fecha_retiro DATE, estado_producto VARCHAR2,cedula_cliente NUMBER, tipo_producto NUMBER) IS
 BEGIN
-UPDATE Producto p SET p.fecha_inicio= fecha_inicio WHERE p.ID_PRODUCTO= id;
+UPDATE Producto c SET c.fecha_inicio = fecha_inicio, c.fecha_retiro = fecha_retiro, c.estado_producto= estado_producto, c.cedula_cliente= cedula_cliente, c.tipo_producto=tipo_producto WHERE c.ID_PRODUCTO=id;
 EXCEPTION
 WHEN OTHERS THEN
-RAISE_APPLICATION_ERROR(-20000,'Error al actualizar la fecha de inicio de un registro en la tabla producto .'||SQLCODE);
-END pActualizarFechaInicio;
+RAISE_APPLICATION_ERROR(-20000,'Error al actualizar  un registro en la tabla Producto.'||SQLERRM);
+END pModificar;
 
 
-PROCEDURE pActualizarFechaRetiro(id NUMBER, fecha_retiro DATE) IS
-BEGIN
-UPDATE Producto p SET p.fecha_retiro =  fecha_retiro  WHERE p.ID_PRODUCTO= id;
-EXCEPTION
-WHEN OTHERS THEN
-RAISE_APPLICATION_ERROR(-20000,'Error al actualizar la fecha de inicio de un registro en la tabla producto .'||SQLCODE);
-END pActualizarFechaRetiro;
-                        
-PROCEDURE pActualizarEstado(id NUMBER, estado_producto VARCHAR2) IS
-BEGIN
-UPDATE Producto p SET p.estado_producto= estado_producto WHERE p.ID_PRODUCTO= id;
-EXCEPTION
-WHEN OTHERS THEN
-RAISE_APPLICATION_ERROR(-20000,'Error al actualizar el estado de un registro en la tabla producto .'||SQLCODE);
-END pActualizarEstado;
-
-FUNCTION  fBuscarProducto(id NUMBER) RETURN producto%rowtype   IS
+FUNCTION  fConsultar(id NUMBER) RETURN producto%rowtype   IS
 prod producto%rowtype;
 BEGIN
 SELECT * into prod
@@ -61,7 +44,7 @@ RETURN prod;
 EXCEPTION
 WHEN OTHERS THEN
 RAISE_APPLICATION_ERROR(-20000,'Error al consultar un registro en la tabla Producto.'||SQLCODE);
-END fBuscarProducto;
+END fConsultar;
 
 END pkProducto;
 

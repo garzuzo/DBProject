79f52cdd273e7877c5ffb4f9c9ca1516a990f02f
garzuzo <garzuxo@gmail.com>
2018-05-27 10:01:03 -0500

    cambios registro nivel2

--- pkNivel2/pkRegistroNivel2.sql
+++ pkNivel2/pkRegistroNivel2.sql
@@ -8,7 +8,7 @@ PROCEDURE pSolicitudReclamo(id_producto NUMBER, id_solicitud NUMBER,fecha_solici
 PROCEDURE pSolicitudCancelacion(id_producto NUMBER, id_solicitud NUMBER,fecha_solicitud Date, estado_atencion VARCHAR, cliente_cedula NUMBER,idProducto NUMBER, observacion VARCHAR);
 
 FUNCTION clienteExiste(cliente_cedula NUMBER) RETURN BOOLEAN;
-
+FUNCTION productoExiste(id_producto NUMBER)RETURN BOOLEAN;
 END pkRegistroNivel2;
 /
 --cuerpo paquete segundo nivel
@@ -50,6 +50,25 @@ WHEN OTHERS THEN
 resultado:= false;
 RETURN resultado;
 END clienteExiste;
+
+
+
+FUNCTION productoExiste(id_producto NUMBER)RETURN BOOLEAN IS
+rcProducto Producto%rowtype;
+resultado boolean;
+BEGIN
+resultado:=true;
+rcProducto:= pkProducto.fConsultar(id_producto);
+EXCEPTION 
+WHEN OTHERS THEN
+resultado:= false;
+RETURN resultado;
+
+END productoExiste;
+
+
+
+
 --
 PROCEDURE pSolicitudModificar(id_producto NUMBER, id_solicitud NUMBER, Observacion Varchar, fecha_solicitud Date,estado_atencion VARCHAR, cliente_cedula NUMBER) IS
 rcCliente Cliente%rowtype;
@@ -58,7 +77,7 @@ rcProducto Producto%rowtype;
 BEGIN
 rcCliente := pkCliente.fConsultar(cliente_cedula);
 rcProducto := pkProducto.fBuscarProducto(id_producto);
-IF(rcCliente!=NULL AND rcProducto!=NULL) THEN
+IF(clienteExiste(cliente_cedula) AND productoExiste(id_producto)) THEN
 pkSolicitud.pInsertarSolicitud(id_solicitud, Observacion, fecha_solicitud, null,estado_atencion, cliente_cedula,2,1, id_producto);
 END IF;
 EXCEPTION
@@ -70,22 +89,52 @@ PROCEDURE pSolicitudDanio(id_producto NUMBER, id_solicitud NUMBER,fecha_solicitu
 
 BEGIN 
 --id_producto NUMBER, id_solicitud NUMBER,fecha_solicitud Date, fecha_atencion DATE,estado_atencion VARCHAR, cliente_cedula NUMBER
+rcCliente := pkCliente.fConsultar(cliente_cedula);
+rcProducto := pkProducto.fBuscarProducto(id_producto);
+
+IF(clienteExiste(cliente_cedula) AND productoExiste(id_producto)) THEN
 pkSolicitud.pInsertarSolicitud(id_solicitud, descripcion , fecha_solicitud, null ,estado_atencion , cliente_cedula ,3,1 , idProducto );
 pkdanio.insertar(idDanio,descripcion, tipo);
 pktipo_anomalia.insertar(nombreAnomalia);
+
+
+END IF;
+EXCEPTION
+WHEN OTHERS THEN
+RAISE_APPLICATION_ERROR(-20000,'Error al registrar una solicitud de modificacion de un producto.'||SQLERRM);
 END pSolicitudDanio;
 
 
 PROCEDURE pSolicitudReclamo(id_producto NUMBER, id_solicitud NUMBER,fecha_solicitud Date, estado_atencion VARCHAR, cliente_cedula NUMBER,idProducto NUMBER, observacion VARCHAR) IS
 BEGIN
+rcCliente := pkCliente.fConsultar(cliente_cedula);
+rcProducto := pkProducto.fBuscarProducto(id_producto);
+IF(clienteExiste(cliente_cedula) AND productoExiste(id_producto)) THEN
 pkSolicitud.pInsertarSolicitud(id_solicitud, observacion , fecha_solicitud, null ,estado_atencion , cliente_cedula ,4,1 , idProducto );
+
+
+END IF;
+EXCEPTION
+WHEN OTHERS THEN
+RAISE_APPLICATION_ERROR(-20000,'Error al registrar una solicitud de reclamo de un producto.'||SQLERRM);
+
 
 END pSolicitudReclamo;
 
 PROCEDURE pSolicitudCancelacion(id_producto NUMBER, id_solicitud NUMBER,fecha_solicitud Date, estado_atencion VARCHAR, cliente_cedula NUMBER,idProducto NUMBER, observacion VARCHAR) IS
 BEGIN
+rcCliente := pkCliente.fConsultar(cliente_cedula);
+rcProducto := pkProducto.fBuscarProducto(id_producto);
+IF(clienteExiste(cliente_cedula) AND productoExiste(id_producto)) THEN
 pkSolicitud.pInsertarSolicitud(id_solicitud, observacion , fecha_solicitud, null ,estado_atencion , cliente_cedula ,5,1 , idProducto );
 
+END IF;
+EXCEPTION
+WHEN OTHERS THEN
+RAISE_APPLICATION_ERROR(-20000,'Error al registrar una solicitud de modificacion de un producto.'||SQLERRM);
+
 END pSolicitudCancelacion;
+
+
 END pkRegistroNivel2;
 /

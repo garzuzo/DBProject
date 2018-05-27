f5932d86fdf7bc02769ceb8eaa070b9af4b6655c
mishale22 <sami_na11@hotmail.com>
2018-05-27 13:27:48 -0500

    paquete registro tercer nivel semilisto

--- pkNivel3/pkRegistroNivel3.sql
+++ pkNivel3/pkRegistroNivel3.sql
@@ -1,31 +1,41 @@
 --especificacion paquete registro tercer nivel
 CREATE OR REPLACE PACKAGE pkRegistroNivel3 AS
-PROCEDURE pSolicitudCreacionN3(tipo_producto NUMBER, id_solicitud NUMBER, Observacion Varchar, fecha_solicitud Date, estado_atencion VARCHAR, cliente_cedula NUMBER);
-PROCEDURE pSolicitudModificarN3(id_producto NUMBER, id_solicitud NUMBER, Observacion Varchar, fecha_solicitud Date, estado_atencion VARCHAR, cliente_cedula NUMBER);
+--SE AÑADIRAN 2 ATRIBUTOS 
+--CODE CODIGO DE LA EXCEPCION
+--MSG MENSAJE DE LA EXCEPCION
+PROCEDURE pSolicitudCreacionN3(tipo_producto NUMBER, id_solicitud NUMBER, observacion VARCHAR, fecha_solicitud DATE, estado_atencion VARCHAR, cliente_cedula NUMBER);
+
+PROCEDURE pSolicitudCancelacionN3(id_producto NUMBER, id_solicitud NUMBER, observacion VARCHAR, fecha_solicitud DATE, estado_atencion VARCHAR, cliente_cedula NUMBER);
+
+PROCEDURE pSolicitudReclamoN3(id_producto NUMBER, id_solicitud NUMBER, observacion VARCHAR, fecha_solicitud DATE, estado_atencion VARCHAR, cliente_cedula NUMBER);
 END pkRegistroNivel3;
 /
 --cuerpo paquete tercer nivel
 CREATE OR REPLACE PACKAGE BODY pkRegistroNivel3 AS
 --registrar solicitud de creación. Los dos primeros parametros son para manejar las excepciones. 
-PROCEDURE pSolicitudCreacionN3(code VARCHAR, msg VARCHAR, atipo_producto NUMBER, id_solicitud NUMBER, Observacion Varchar, fecha_solicitud Date, fecha_atencion DATE,estado_atencion VARCHAR, cliente_cedula NUMBER) IS
-BEGIN 
-pkRegistroNivel2.pRegistroSolicitudCreacion(atipo_producto, id_solicitud, Observacion, fecha_solicitud, estado_atencion, cliente_cedula);
+PROCEDURE pSolicitudCreacionN3(tipo_producto NUMBER, id_solicitud NUMBER, observacion VARCHAR, fecha_solicitud DATE, estado_atencion VARCHAR, cliente_cedula NUMBER) IS
+BEGIN
+pkRegistroN2.pSolicitudCreacion(tipo_producto, id_solicitud, observacion , fecha_solicitud , estado_atencion , cliente_cedula );
 EXCEPTION
 WHEN OTHERS THEN
-code := SQLERRC || '.';
-msg := ERRMSG || '.';
---RAISE_APPLICATION_ERROR(-20000,'Error al registrar una solicitud de creación.'||SQLERRM);
+RAISE_APPLICATION_ERROR(-20000,'Error al registrar una solicitud de creacion. '||SQLERRM);
 END pSolicitudCreacionN3;
 
 --
-PROCEDURE pSolicitudModificarN3(code VARCHAR, msg VARCHAR, id_producto NUMBER, id_solicitud NUMBER, Observacion Varchar, fecha_solicitud Date, estado_atencion VARCHAR, cliente_cedula NUMBER) IS
+PROCEDURE pSolicitudCancelacionN3(id_producto NUMBER, id_solicitud NUMBER, observacion VARCHAR, fecha_solicitud DATE, estado_atencion VARCHAR, cliente_cedula NUMBER) IS
 BEGIN
-pkRegistroNivel2.pSolicitudModificar(id_producto, id_solicitud, Observacion, fecha_solicitud,estado_atencion, cliente_cedula);
+pkRegistroN2.pSolicitudCancelacion(id_producto, id_solicitud, observacion, fecha_solicitud, estado_atencion, cliente_cedula);
 EXCEPTION
 WHEN OTHERS THEN
-code := SQLERRC || '.';
-msg := ERRMSG || '.';
---RAISE_APPLICATION_ERROR(-20000,'Error al registrar una solicitud de modificación de un producto.'||SQLERRM);
-END pSolicitudModificarN3;
+RAISE_APPLICATION_ERROR(-20000,'Error al registrar una solicitud de cancelacion. '||SQLERRM);
+END pSolicitudCancelacionN3;
+
+PROCEDURE pSolicitudReclamoN3(id_producto NUMBER, id_solicitud NUMBER, observacion VARCHAR, fecha_solicitud DATE, estado_atencion VARCHAR, cliente_cedula NUMBER) IS
+BEGIN
+pkRegistroN2.pSolicitudReclamo(id_producto, id_solicitud, observacion, fecha_solicitud, estado_atencion, cliente_cedula);
+EXCEPTION
+WHEN OTHERS THEN
+RAISE_APPLICATION_ERROR(-20000,'Error al registrar una solicitud de reclamo. '|| SQLERRM);
+END pSolicitudReclamoN3;
 
 END pkRegistroNivel3;

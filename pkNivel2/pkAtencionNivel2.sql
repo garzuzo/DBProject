188070e6305a4c08e9f3e517caa225792840bb4d
juan <juanfe_518@hotmail.com>
2018-05-26 21:39:24 -0500

    Update AtencionNuevoProducto, AtencionRetiroProducto

--- pkNivel2/pkAtencionNivel2.sql
+++ pkNivel2/pkAtencionNivel2.sql
@@ -1,97 +1,101 @@
 --Creacion de especificacion del paquete
-CREATE OR REPLACE PACKAGE pkAtencionNivel2 AS -- spec
-PROCEDURE pSolicitudNuevoProducto(idTSol NUMBER, nomTSol varchar);
-PROCEDURE pSolicitudRetiroProducto(idSolicitud NUMBER, observacion VARCHAR) ;
-PROCEDURE pSolicitudDanosReclamos(idSolicitud NUMBER, observacion VARCHAR, estado VARCHAR);
+CREATE OR REPLACE PACKAGE pkAtencionNivel2
+AS -- spec
+  PROCEDURE pSolicitudNuevoProducto(
+      idSolicitud NUMBER);
+  PROCEDURE pSolicitudRetiroProducto(
+      idSolicitud NUMBER,
+      observacion VARCHAR) ;
+  PROCEDURE pSolicitudDanosReclamos(
+      idSolicitud NUMBER,
+      observacion VARCHAR,
+      estado      VARCHAR);
 END pkAtencionNivel2;
 /
-
-CREATE OR REPLACE PACKAGE BODY pkAtencionNivel2 AS -- creacion cuerpo del paquete
-
---Procedimiento para atender una solicitud de nuevo producto
-PROCEDURE pSolicitudNuevoProducto(idProduct NUMBER, FechaInicio Date, FechaRetiro Date, EstadoProducto varchar, Cliente NUMBER,TpoProducto NUMBER);
-
-      Begin
-
-        IF idProduct IS NULL THEN
-           RAISE_APPLICATION_ERROR(-20001, 'Ingrese un id  válido');   
-        END IF;
-        
-        IF FechaInicio  IS NULL THEN
-            RAISE_APPLICATION_ERROR(-20001, 'Ingrese un id  válido');  
-        END IF ;
-        
-        IF FechaRetiro  IS NULL THEN
-            RAISE_APPLICATION_ERROR(-20001, 'Ingrese un id  válido');  
-        END IF; 
-        
-         IF EstadoProducto  IS NULL THEN
-            RAISE_APPLICATION_ERROR(-20001, 'Ingrese un id  válido');  
-        END IF;
-        
-        IF Cliente  IS NULL THEN
-            RAISE_APPLICATION_ERROR(-20001, 'Ingrese un id  válido');  
-        END IF;
-        
-        IF TpoProducto  IS NULL THEN
-            RAISE_APPLICATION_ERROR(-20001, 'Ingrese un id  válido');  
-        END IF;
-        
-       
-        
-    PKPRODUCTO.PINSERTAR(idProduct,FechaInicio,FechaRetiro,EstadoProducto,EstadoProducto,Cliente,TpoProducto);
-end pSolicitudNuevoProducto;
-
---Procedimiento para atender una solicitud de retiro 
-PROCEDURE pSolicitudRetiroProducto(idSolicitud NUMBER, observacion VARCHAR) IS
-solic SOLICITUD%rowtype;
+CREATE OR REPLACE PACKAGE BODY pkAtencionNivel2
+AS -- creacion cuerpo del paquete
+  --Procedimiento para atender una solicitud de nuevo producto
+PROCEDURE pSolicitudNuevoProducto(
+    idSolicitud NUMBER)
+IS
+  fechaInicio DATE;
+  solic SOLICITUD%rowtype;
 BEGIN
-         
-        IF idSolicitud IS NULL THEN
-        RAISE_APPLICATION_ERROR (-20001, 'ingrese el id de la solicitud');
-         END IF;
-        
-        IF observacion IS NULL THEN
-        RAISE_APPLICATION_ERROR (-20001, 'ingrese observacion de la solicitud');
-         END IF;
-        
-        solic := PKSOLICITUD.FCONSULTARSOLICITUD(idSolicitud);
-        
-        --Actualiza fecha de retiro de producto
-        PKPRODUCTO.PACTUALIZARFECHARETIRO(solic.PRODUCTO_ID, SYSDATE);
-        
-        --Actualiza el estado de la solicitud.
-        PKSOLICITUD.PMODIFICARSOLICITUD(solic.ID, observacion, solic.FECHA_SOLICITUD, SYSDATE, 
-                                'ATENDIDA', solic.CLIENTE_CEDULA, solic.TIPOSOLICITUD_ID, solic.FUNCIONARIO_ID);     
-    
+--valida el parametro
+  IF idSolicitud IS NULL THEN
+    RAISE_APPLICATION_ERROR(-20001, 'Ingrese un id  válido');
+  END IF;
+  
+  --Obtiene la solicitud
+  solic := PKSOLICITUD.FCONSULTARSOLICITUD(idSolicitud);
+  
+  --Actualiza el estado del producto a activo
+  PKPRODUCTO.PACTUALIZARESTADO( solic.PRODUCTO_ID_PRODUCTO , 'ACTIVO');
+  
+  --setea la fecha inicio para el primer dia del mes siquiente al actual.
+  fechaInicio := TO_DATE('''' || EXTRACT(YEAR, SYSDATE) || (EXTRACT(MONTH,
+  SYSDATE))+1 || '1' || '''', 'yyyy/mm/dd');
+  
+  --Actualiza al fecha de inicio 
+  PKPRODUCTO.PACTUALIZARFECHAINICIO( solic.PRODUCTO_ID_PRODUCTO , fechaInicio);
+  
+  --Actualiza el estado de la solicitud.
+  PKSOLICITUD.PMODIFICARSOLICITUD(solic.ID, observacion, solic.FECHA_SOLICITUD,
+  SYSDATE, 'ATENDIDA', solic.CLIENTE_CEDULA, solic.TIPOSOLICITUD_ID,
+  solic.FUNCIONARIO_ID);
+END pSolicitudNuevoProducto;
+--Procedimiento para atender una solicitud de retiro
+PROCEDURE pSolicitudRetiroProducto(
+    idSolicitud NUMBER,
+    observacion VARCHAR)
+IS
+  solic SOLICITUD%rowtype;
+BEGIN
+  IF idSolicitud IS NULL THEN
+    RAISE_APPLICATION_ERROR (-20001, 'ingrese el id de la solicitud');
+  END IF;
+  IF observacion IS NULL THEN
+    RAISE_APPLICATION_ERROR (-20001, 'ingrese observacion de la solicitud');
+  END IF;
+  
+  --Obtiene la solicitud
+  solic := PKSOLICITUD.FCONSULTARSOLICITUD(idSolicitud);
+  
+  --Actualiza el estado del producto a activo
+  PKPRODUCTO.PACTUALIZARESTADO( solic.PRODUCTO_ID_PRODUCTO , 'INACTIVO');
+  
+  --Actualiza fecha de retiro de producto
+  PKPRODUCTO.PACTUALIZARFECHARETIRO(solic.PRODUCTO_ID, SYSDATE);
+  
+  --Actualiza el estado de la solicitud.
+  PKSOLICITUD.PMODIFICARSOLICITUD(solic.ID, observacion, solic.FECHA_SOLICITUD,
+  SYSDATE, 'ATENDIDA', solic.CLIENTE_CEDULA, solic.TIPOSOLICITUD_ID,
+  solic.FUNCIONARIO_ID);
+  
+  
 END pSolicitudRetiroProducto;
-
-
 --Procedimiento para atender danos y reclamos
-PROCEDURE pSolicitudDanosReclamos(idSolicitud NUMBER, observacion VARCHAR, estado VARCHAR) IS
-solic SOLICITUD%rowtype;
+PROCEDURE pSolicitudDanosReclamos(
+    idSolicitud NUMBER,
+    observacion VARCHAR,
+    estado      VARCHAR)
+IS
+  solic SOLICITUD%rowtype;
 BEGIN
-         
-        IF idSolicitud IS NULL THEN
-        RAISE_APPLICATION_ERROR (-20001, 'ingrese el id de la solicitud');
-        END IF; 
-        
-        IF observacion IS NULL THEN
-        RAISE_APPLICATION_ERROR (-20001, 'ingrese observacion de la solicitud');
-        END IF; 
-        
-        IF estado IS NULL THEN
-        RAISE_APPLICATION_ERROR (-20001, 'ingrese un estado para la solicitud');
-        END IF; 
-        
-        --obtiene la solicitud
-        solic := PKSOLICITUD.FCONSULTARSOLICITUD(idSolicitud);        
-            
-        --Actualiza el estado de la solicitud.
-        PKSOLICITUD.PMODIFICARSOLICITUD(solic.ID, observacion, solic.FECHA_SOLICITUD, SYSDATE, 
-                                estado, solic.CLIENTE_CEDULA, solic.TIPOSOLICITUD_ID, solic.FUNCIONARIO_ID);
-        
+  IF idSolicitud IS NULL THEN
+    RAISE_APPLICATION_ERROR (-20001, 'ingrese el id de la solicitud');
+  END IF;
+  IF observacion IS NULL THEN
+    RAISE_APPLICATION_ERROR (-20001, 'ingrese observacion de la solicitud');
+  END IF;
+  IF estado IS NULL THEN
+    RAISE_APPLICATION_ERROR (-20001, 'ingrese un estado para la solicitud');
+  END IF;
+  --obtiene la solicitud
+  solic := PKSOLICITUD.FCONSULTARSOLICITUD(idSolicitud);
+  --Actualiza el estado de la solicitud.
+  PKSOLICITUD.PMODIFICARSOLICITUD(solic.ID, observacion, solic.FECHA_SOLICITUD,
+  SYSDATE, estado, solic.CLIENTE_CEDULA, solic.TIPOSOLICITUD_ID,
+  solic.FUNCIONARIO_ID);
 END pSolicitudDanosReclamos;
-
-
 END pkAtencionNivel2;

96e5430acd51dc27be4df5efc6866f6e0d2da2f8
garzuzo <garzuxo@gmail.com>
2018-05-27 12:47:37 -0500

    cambios pkProducto

--- pkNivel1/pkProducto.sql
+++ pkNivel1/pkProducto.sql
@@ -1,44 +1,45 @@
 CREATE OR REPLACE PACKAGE pkProducto AS
-PROCEDURE pInsertar(id NUMBER, fecha_inicio DATE, fecha_retiro DATE, estado_producto VARCHAR2,cedula_cliente NUMBER, tipo_producto NUMBER);
-PROCEDURE pEliminar(id NUMBER);
-PROCEDURE pModificar(id NUMBER, fecha_inicio DATE);
-FUNCTION  fConsultar(id NUMBER) RETURN producto%rowtype;
+PROCEDURE pInsertar(ID_PRODUCTO NUMBER, fecha_inicio DATE, fecha_retiro DATE, estado_producto VARCHAR2,cedula_cliente NUMBER, tipo_producto NUMBER);
+PROCEDURE pBorrar(ID_PRODUCTO NUMBER);
+PROCEDURE pModificar(ID_PRODUCTO NUMBER, fecha_inicio DATE, fecha_retiro DATE, estado_producto VARCHAR2,cedula_cliente NUMBER, tipo_producto NUMBER);
+FUNCTION fConsultar(ID_PRODUCTO NUMBER) RETURN producto%rowtype;
 END pkProducto;
 /
 create or replace PACKAGE BODY pkProducto AS
 
-PROCEDURE pInsertar(id NUMBER, fecha_inicio DATE, fecha_retiro DATE, estado_producto VARCHAR2,cedula_cliente NUMBER, tipo_producto NUMBER) IS
+PROCEDURE pInsertar(ID_PRODUCTO NUMBER, fecha_inicio DATE, fecha_retiro DATE, estado_producto VARCHAR2,cedula_cliente NUMBER, tipo_producto NUMBER) IS
 BEGIN 
-INSERT INTO Producto VALUES(id , fecha_inicio , fecha_retiro , estado_producto, cedula_cliente,tipo_producto);
+INSERT INTO Producto VALUES(ID_PRODUCTO , fecha_inicio , fecha_retiro , estado_producto, cedula_cliente,tipo_producto);
 EXCEPTION
 WHEN OTHERS THEN
 RAISE_APPLICATION_ERROR(-20000,'Error al insertar en la tabla Producto.'||SQLCODE);
 END pInsertar;
 
-PROCEDURE pEliminar(id NUMBER) IS
+PROCEDURE pBorrar(ID_PRODUCTO NUMBER) IS
 BEGIN
-DELETE FROM Producto p WHERE p.ID_PRODUCTO = id;
+DELETE FROM Producto pro WHERE pro.ID_PRODUCTO = ID_PRODUCTO;
 EXCEPTION
 WHEN OTHERS THEN
-RAISE_APPLICATION_ERROR(-20000,'Error al eliminar en la tabla Producto.'||SQLCODE);
-END pEliminar;
+RAISE_APPLICATION_ERROR(-20000,'Error al eliminar en la tabla Cliente.'||SQLERRM);
+END pBorrar;
 
 
-PROCEDURE pModificar(id NUMBER, fecha_inicio DATE, fecha_retiro DATE, estado_producto VARCHAR2,cedula_cliente NUMBER, tipo_producto NUMBER) IS
+PROCEDURE pModificar(ID_PRODUCTO NUMBER, fecha_inicio DATE, fecha_retiro DATE, estado_producto VARCHAR2,cedula_cliente NUMBER, tipo_producto NUMBER) IS
 BEGIN
-UPDATE Producto c SET c.fecha_inicio = fecha_inicio, c.fecha_retiro = fecha_retiro, c.estado_producto= estado_producto, c.cedula_cliente= cedula_cliente, c.tipo_producto=tipo_producto WHERE c.ID_PRODUCTO=id;
+UPDATE Producto pro SET pro.fecha_inicio = fecha_inicio, pro.fecha_retiro = fecha_retiro, pro.estado_producto= estado_producto, pro.cedula_cliente= cedula_cliente, pro.tipo_producto=tipo_producto WHERE pro.ID_PRODUCTO=ID_PRODUCTO;
 EXCEPTION
 WHEN OTHERS THEN
 RAISE_APPLICATION_ERROR(-20000,'Error al actualizar  un registro en la tabla Producto.'||SQLERRM);
 END pModificar;
 
 
-FUNCTION  fConsultar(id NUMBER) RETURN producto%rowtype   IS
+
+FUNCTION fConsultar(ID_PRODUCTO NUMBER) RETURN producto%rowtype   IS
 prod producto%rowtype;
 BEGIN
 SELECT * into prod
 FROM Producto p
-WHERE p.ID_PRODUCTO=id;
+WHERE p.ID_PRODUCTO=ID_PRODUCTO;
 
 RETURN prod;
 EXCEPTION
@@ -47,6 +48,6 @@ RAISE_APPLICATION_ERROR(-20000,'Error al consultar un registro en la tabla Produ
 END fConsultar;
 
 END pkProducto;
-
+/
 --EXECUTE pkProducto.pInsertar(1234 NUMBER, 10-03-15,  10-06-15, 'activo');
 

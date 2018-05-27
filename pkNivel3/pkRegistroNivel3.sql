03e8f2db9d7018a89b3e84785908f24670f01a1d
garzuzo <garzuxo@gmail.com>
2018-05-27 14:13:22 -0500

    finalizacion pkRegistroNivel3.sql

--- pkNivel3/pkRegistroNivel3.sql
+++ pkNivel3/pkRegistroNivel3.sql
@@ -1,6 +1,6 @@
 --especificacion paquete registro tercer nivel
 CREATE OR REPLACE PACKAGE pkRegistroNivel3 AS
---SE AÑADIRAN 2 ATRIBUTOS 
+--SE Aï¿½ADIRAN 2 ATRIBUTOS 
 --CODE CODIGO DE LA EXCEPCION
 --MSG MENSAJE DE LA EXCEPCION
 PROCEDURE pSolicitudCreacionN3(tipo_producto NUMBER, id_solicitud NUMBER, observacion VARCHAR, fecha_solicitud DATE, estado_atencion VARCHAR, cliente_cedula NUMBER);
@@ -8,11 +8,17 @@ PROCEDURE pSolicitudCreacionN3(tipo_producto NUMBER, id_solicitud NUMBER, observ
 PROCEDURE pSolicitudCancelacionN3(id_producto NUMBER, id_solicitud NUMBER, observacion VARCHAR, fecha_solicitud DATE, estado_atencion VARCHAR, cliente_cedula NUMBER);
 
 PROCEDURE pSolicitudReclamoN3(id_producto NUMBER, id_solicitud NUMBER, observacion VARCHAR, fecha_solicitud DATE, estado_atencion VARCHAR, cliente_cedula NUMBER);
+
+PROCEDURE pSolicitudDanioN3(id_producto NUMBER,id_danio NUMBER,id_anomalia NUMBER, id_solicitud NUMBER, observacion VARCHAR, fecha_solicitud DATE, estado_atencion VARCHAR, cliente_cedula NUMBER);
+
+PROCEDURE pSolicitudModificacionN3(id_producto NUMBER,id_tipo_producto_nuevo NUMBER, id_solicitud NUMBER, observacion VARCHAR, fecha_inicio DATE,fecha_solicitud DATE, estado_atencion VARCHAR, cliente_cedula NUMBER);
+
+
 END pkRegistroNivel3;
 /
 --cuerpo paquete tercer nivel
 CREATE OR REPLACE PACKAGE BODY pkRegistroNivel3 AS
---registrar solicitud de creación. Los dos primeros parametros son para manejar las excepciones. 
+--registrar solicitud de creacion. Los dos primeros parametros son para manejar las excepciones. 
 PROCEDURE pSolicitudCreacionN3(tipo_producto NUMBER, id_solicitud NUMBER, observacion VARCHAR, fecha_solicitud DATE, estado_atencion VARCHAR, cliente_cedula NUMBER) IS
 BEGIN
 pkRegistroN2.pSolicitudCreacion(tipo_producto, id_solicitud, observacion , fecha_solicitud , estado_atencion , cliente_cedula );
@@ -37,5 +43,21 @@ EXCEPTION
 WHEN OTHERS THEN
 RAISE_APPLICATION_ERROR(-20000,'Error al registrar una solicitud de reclamo. '|| SQLERRM);
 END pSolicitudReclamoN3;
+
+PROCEDURE pSolicitudDanioN3(id_producto NUMBER,id_danio NUMBER,id_anomalia NUMBER, id_solicitud NUMBER, observacion VARCHAR, fecha_solicitud DATE, estado_atencion VARCHAR, cliente_cedula NUMBER) IS
+BEGIN
+pkRegistroN2.pSolicitudDanio(id_producto ,id_danio ,id_anomalia , id_solicitud , observacion , fecha_solicitud , estado_atencion , cliente_cedula );
+EXCEPTION
+WHEN OTHERS THEN
+RAISE_APPLICATION_ERROR(-20000,'Error al registrar una solicitud de dano. '|| SQLERRM);
+END pSolicitudDanioN3;
+
+PROCEDURE pSolicitudModificacionN3(id_producto NUMBER,id_tipo_producto_nuevo NUMBER, id_solicitud NUMBER, observacion VARCHAR, fecha_inicio DATE,fecha_solicitud DATE, estado_atencion VARCHAR, cliente_cedula NUMBER) IS
+BEGIN
+pkRegistroN2.pSolicitudModificacion(id_producto ,id_tipo_producto_nuevo , id_solicitud , observacion , fecha_inicio ,fecha_solicitud , estado_atencion , cliente_cedula );
+EXCEPTION
+WHEN OTHERS THEN
+RAISE_APPLICATION_ERROR(-20000,'Error al registrar una solicitud de modificacion. '|| SQLERRM);
+END pSolicitudModificacionN3;
 
 END pkRegistroNivel3;

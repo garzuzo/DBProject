754cf354ac8137f33e131a199bedb0cc799577b4
garzuzo <garzuxo@gmail.com>
2018-05-27 02:22:25 -0500

    cambios paquete nivel 2

--- pkNivel2/pkRegistroNivel2.sql
+++ pkNivel2/pkRegistroNivel2.sql
@@ -1,17 +1,22 @@
---especificación paquete segundo nivel
+--especificacion paquete segundo nivel
 
 CREATE OR REPLACE PACKAGE pkRegistroNivel2 AS
-PROCEDURE pRegistrarSolicitudCreacion(tipo_producto NUMBER, id_solicitud NUMBER, Observacion Varchar, fecha_solicitud Date, fecha_atencion DATE,estado_atencion VARCHAR, cliente_cedula NUMBER);
-PROCEDURE pSolicitudModificar(id_producto NUMBER, id_solicitud NUMBER, Observacion Varchar, fecha_solicitud Date, fecha_atencion DATE,estado_atencion VARCHAR, cliente_cedula NUMBER);
---PROCEDURE pSolicitudDanio();
---PROCEDURE pSolicitudReclamo();
---PROCEDURE pSolicitudCancelacion();
+PROCEDURE pRegistrarSolicitudCreacion(tipo_producto NUMBER, id_solicitud NUMBER, Observacion Varchar, fecha_solicitud Date,estado_atencion VARCHAR, cliente_cedula NUMBER);
+PROCEDURE pSolicitudModificar(id_producto NUMBER, id_solicitud NUMBER, Observacion Varchar, fecha_solicitud Date,estado_atencion VARCHAR, cliente_cedula NUMBER);
+PROCEDURE pSolicitudDanio(id_producto NUMBER, id_solicitud NUMBER,fecha_solicitud Date, estado_atencion VARCHAR, cliente_cedula NUMBER,idDanio NUMBER,descripcion VARCHAR, idAnomalia NUMBER, nombreAnomalia VARCHAR);
+PROCEDURE pSolicitudReclamo(id_producto NUMBER, id_solicitud NUMBER,fecha_solicitud Date, estado_atencion VARCHAR, cliente_cedula NUMBER,idProducto NUMBER, observacion VARCHAR);
+PROCEDURE pSolicitudCancelacion(id_producto NUMBER, id_solicitud NUMBER,fecha_solicitud Date, estado_atencion VARCHAR, cliente_cedula NUMBER,idProducto NUMBER, observacion VARCHAR);
+
+FUNCTION clienteExiste(cliente_cedula NUMBER) RETURN BOOLEAN;
+
 END pkRegistroNivel2;
 /
 --cuerpo paquete segundo nivel
 CREATE OR REPLACE PACKAGE BODY pkRegistroNivel2 AS
---registrar solicitud de creación
-PROCEDURE pRegistrarSolicitudCreacion(tipo_producto NUMBER, id_solicitud NUMBER, Observacion Varchar, fecha_solicitud Date, fecha_atencion DATE,estado_atencion VARCHAR, cliente_cedula NUMBER) IS
+
+
+--registrar solicitud de creacion
+PROCEDURE pRegistrarSolicitudCreacion(tipo_producto NUMBER, id_solicitud NUMBER, Observacion Varchar, fecha_solicitud Date,estado_atencion VARCHAR, cliente_cedula NUMBER) IS
 rcCliente Cliente%rowtype;
 numProductos NUMBER;
 numActual NUMBER;
@@ -31,7 +36,7 @@ pkSolicitud.pInsertarSolicitud(id_solicitud, Observacion, fecha_solicitud, null,
 END IF;
 EXCEPTION
 WHEN OTHERS THEN
-RAISE_APPLICATION_ERROR(-20000,'Error al registrar una solicitud de creación.'||SQLERRM);
+RAISE_APPLICATION_ERROR(-20000,'Error al registrar una solicitud de creacion.'||SQLERRM);
 END pRegistrarSolicitudCreacion;
 
 FUNCTION clienteExiste(cliente_cedula NUMBER) RETURN BOOLEAN IS
@@ -46,7 +51,7 @@ resultado:= false;
 RETURN resultado;
 END clienteExiste;
 --
-PROCEDURE pSolicitudModificar(id_producto NUMBER, id_solicitud NUMBER, Observacion Varchar, fecha_solicitud Date, fecha_atencion DATE,estado_atencion VARCHAR, cliente_cedula NUMBER) IS
+PROCEDURE pSolicitudModificar(id_producto NUMBER, id_solicitud NUMBER, Observacion Varchar, fecha_solicitud Date,estado_atencion VARCHAR, cliente_cedula NUMBER) IS
 rcCliente Cliente%rowtype;
 rcProducto Producto%rowtype;
 
@@ -58,8 +63,29 @@ pkSolicitud.pInsertarSolicitud(id_solicitud, Observacion, fecha_solicitud, null,
 END IF;
 EXCEPTION
 WHEN OTHERS THEN
-RAISE_APPLICATION_ERROR(-20000,'Error al registrar una solicitud de modificación de un producto.'||SQLERRM);
+RAISE_APPLICATION_ERROR(-20000,'Error al registrar una solicitud de modificacion de un producto.'||SQLERRM);
 END pSolicitudModificar;
 
+PROCEDURE pSolicitudDanio(id_producto NUMBER, id_solicitud NUMBER,fecha_solicitud Date,estado_atencion VARCHAR, cliente_cedula NUMBER,idDanio NUMBER,descripcion VARCHAR, idAnomalia NUMBER, nombreAnomalia VARCHAR) is
+
+BEGIN 
+--id_producto NUMBER, id_solicitud NUMBER,fecha_solicitud Date, fecha_atencion DATE,estado_atencion VARCHAR, cliente_cedula NUMBER
+pkSolicitud.pInsertarSolicitud(id_solicitud, descripcion , fecha_solicitud, null ,estado_atencion , cliente_cedula ,3,1 , idProducto );
+pkdanio.insertar(idDanio,descripcion, tipo);
+pktipo_anomalia.insertar(nombreAnomalia);
+END pSolicitudDanio;
+
+
+PROCEDURE pSolicitudReclamo(id_producto NUMBER, id_solicitud NUMBER,fecha_solicitud Date, estado_atencion VARCHAR, cliente_cedula NUMBER,idProducto NUMBER, observacion VARCHAR) IS
+BEGIN
+pkSolicitud.pInsertarSolicitud(id_solicitud, observacion , fecha_solicitud, null ,estado_atencion , cliente_cedula ,4,1 , idProducto );
+
+END pSolicitudReclamo;
+
+PROCEDURE pSolicitudCancelacion(id_producto NUMBER, id_solicitud NUMBER,fecha_solicitud Date, estado_atencion VARCHAR, cliente_cedula NUMBER,idProducto NUMBER, observacion VARCHAR) IS
+BEGIN
+pkSolicitud.pInsertarSolicitud(id_solicitud, observacion , fecha_solicitud, null ,estado_atencion , cliente_cedula ,5,1 , idProducto );
+
+END pSolicitudCancelacion;
 END pkRegistroNivel2;
 /

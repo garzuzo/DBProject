b744265046dfe3a621b84729ecc550cae0406c3b
danielq97 <discover_daniel@hotmail.com>
2018-05-27 14:03:31 -0500

    Cambio en el paqueteAsignacionNIvel2

--- pkNivel2/pkAsignacionNivel2.sql
+++ pkNivel2/pkAsignacionNivel2.sql
@@ -1,34 +1,19 @@
 --Creacion de especificacion del paquete
 CREATE OR REPLACE PACKAGE pkAsignacionNivel2 AS -- spec
-PROCEDURE pSolicitudNuevoProducto(idTSol NUMBER, nomTSol varchar);
-PROCEDURE pSolicitudRetiroProducto(idTSol NUMBER) ;
-PROCEDURE pSolicitudDanosReclamos(idTSol NUMBER, nomTSol varchar);
+PROCEDURE pAsignarSolicitudaFuncionario;
+
 
 END pkAsignacionNivel2;
 /
-CREATE OR REPLACE PACKAGE BODY pkAsignacionNivel2 AS -- creacion cuerpo del paquete
+CREATE OR REPLACE PACKAGE BODY pkAsignacionNivel2 AS -- body
 
---Procedimiento para atender una solicitud de nuevo producto
-PROCEDURE pSolicitudNuevoProducto(idTSol NUMBER, nomTSol varchar);
+--Procedimiento para asignar una solicitud a un funcionario
+PROCEDURE pSolicitudNuevoProducto;
 
---Procedimiento para atender una solicitud de retiro 
-PROCEDURE pSolicitudRetiroProducto(idTSol NUMBER) ;
 
---Procedimiento 
-PROCEDURE pSolicitudDanosReclamos(idTSol NUMBER, nomTSol varchar);
 
-PROCEDURE pAsignarSolicitudAFuncionario(id solicitud);
-
-----Encontrar el funcionario disponible para asignarlo a una solicitud
-
-FUNCTION funcionariosDisponibles RETURN FUNCIONARIO.id IS
-         idFuncionario number;
-    BEGIN
-          SELECT * into idFuncionario FROM FUNCIONARIO fun, SOLICITUD sol WHERE fun.id=sol.funcionarioId AND rownum<3;
-          RETURN idFuncionario;
-            
-    
-END funcionariosDisponibles;
 
 
 END pkAsignacionNivel2;
+
+EXECUTE PKFUNCIONARIO.INSERTAR(5,'Antonia');
